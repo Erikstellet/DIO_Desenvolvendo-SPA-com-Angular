d0871cf265a1d4db87f7eb9ca51abe89d2b43f29
Erikstellet <erikstellet@gmail.com>
2021-02-06 09:58:54 -0300

    CSS e Table News

---
+++ src/app/home/test/news.ts
@@ -0,0 +1,7 @@
+export class News
+{
+  id: number | undefined;
+  title: string | undefined;
+  imageUrl: string | undefined;
+  text: string | undefined;
+}
--- src/app/home/test/test.component.ts
+++ src/app/home/test/test.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { News } from './news';
 
 @Component
 ({
@@ -12,9 +13,40 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 export class TestComponent implements OnInit
 {
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  news: News[] = [];
 
-  ngOnInit(): void  { }
+  ngOnInit(): void
+  {
+    this.news =
+    [
+      {
+        id: 1,
+        title: 'Os Filmes de 2021',
+        imageUrl: 'assets/image/thumb/kong-and-godzzila.jpg',
+        text: 'Os mais esperados de 2021'
+      },
+      {
+        id: 2,
+        title: 'Prince of Persia',
+        imageUrl: 'assets/image/thumb/price-of-persia.jpg',
+        text: 'Remake do game é adiado mais uma vez'
+      },
+      {
+        id: 3,
+        title: 'ATTACK ON TITAN',
+        imageUrl: 'assets/image/thumb/atack-on-titan.jpg',
+        text: 'Anime conta o segredo por trás do ataque de Eren'
+      },
+      {
+        id: 4,
+        title: 'ATTACK ON TITAN',
+        imageUrl: 'assets/image/thumb/atack-on-titan.jpg',
+        text: 'Anime conta o segredo por trás do ataque de Eren'
+      }
+    ]
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -32,4 +64,7 @@ export class TestComponent implements OnInit
       ];
     })
   );
+
+
+
 }
